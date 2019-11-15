9db1638d45634190056ec300a21f51a0b29dfd34
bachlin <bachjin@mail.ustc.edu.cn>
2019-11-15 15:05:28 +0800

    frame of bitwise operation

---
+++ logistic.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "decimal.h"
+
+#define PRINT_TIMES 10
+
+
+__uint16_t substitute(__uint16_t mu, __uint16_t x)
+{
+    //represents 3.57 <= mu + 3 < 4, 0 < x < 1
+    __uint32_t temp = x;
+    temp *= (x ^ 0xffff);
+    //  x ^ 0xffff is 1 - x
+
+    temp = temp >> 16;
+    //  the last 16 bit is the fractional part of x*(1-x)
+
+    temp *= (mu >> 2) | 0xc000;
+    //  temp *= (mu + 3), the decimal point is left of 30 bits
+
+    return temp >> 14;
+}
+
+__uint8_t x_output(__uint16_t x)
+{
+    return x >> 8;
+}
+
+int main()
+{
+    __uint16_t mu = 0xc000;
+    __uint16_t x = 0x8000;
+
+    float m_t, x_t;
+    scanf("%f%f", &m_t, &x_t);
+    mu = (m_t - 3)* 65536;
+    x = x_t * 65536;
+    //  mu don't store 3, in other words,
+    //  mu only include the fractional part of real mu
+    
+    for(int i = 1; i <= PRINT_TIMES; i++)
+    {
+        x = substitute(mu, x);
+        
+        printf("%d\n", x_output(x));
+    }
+
+    return 0;
+}
